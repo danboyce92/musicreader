06c096deff8c57db250f42eaa6b3b2f2bf8887c0
Daniel Boyce <daarf92@gmail.com>
2022-11-13 16:27:52 +0000

    onChange not working.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+import React, { useState } from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from './firebase/firebase';
 import Landing from './components/Landing';
 import './styles/App.css';
 
 function App() {
+
+  const[userName, setUserName] = useState({});
+
+
+  const logout = async () => {
+    await signOut(auth);
+}
+
   return (
     <div className="app">
+
       <div className="title">♩♩ Reader</div>
+
       <div className="container">
-        <Landing />
+
+        <button 
+        className="ui semantic big inverted purple button" 
+        id="signOut"
+        onClick={logout}
+        >Signout</button>
+        
+
+        <Landing 
+        changeUserName={userName => setUserName(userName)}
+        />
+
+
+
       </div>
+
+      <h4 className="loggedIn" >user: {userName?.email} </h4>
+      
+      <div className="getLoggedIn">
+                Please Login or Register to play..
+
+            </div>
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebase/firebase';
 import '../styles/Landing.css';
 
-const Landing = ({handleUser}) => {
+const Landing = (props) => {
 
-    const [registerEmail, setRegisterEmail] = useState('');
-    const [registerPassword, setRegisterPassword] = useState('');
-    const [loginEmail, setLoginEmail] = useState('');
-    const [loginPassword, setLoginPassword] = useState('');
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
 
     const [user, setUser] = useState({});
 
     onAuthStateChanged(auth, (currentUser) => {
         setUser(currentUser);
+
     })
 
     const loginButton = async () => {
-        console.log("Hello")
+
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail,
+                loginPassword
+                );
+            } catch (error) {
+                console.log(error.message);
+            }
+
     }
 
     const registerButton = async () => {
@@ -27,88 +38,69 @@ const Landing = ({handleUser}) => {
             registerEmail,
             registerPassword
             );
+            console.log(user)
         } catch (error) {
             console.log(error.message);
         }
     };
 
-    const logout = async () => {
 
-    }
 
     return (
         <div>
 
-    <div className="ui placeholder segment">
-    <div className="ui two column very relaxed stackable grid">
-        <div className="column">
-        <div className="ui form">
-            <div className="field">
-            <label>Email</label>
-            <div className="ui left icon input">
-                <input 
-                type="text" 
-                placeholder="Email"
-                onChange={(event) => {setLoginEmail(event.target.value)}}
-                />
-                <i className="user icon"></i>
-            </div>
-            </div>
-            <div className="field">
-            <label>Password</label>
-            <div className="ui left icon input">
-                <input 
-                type="password"
-                onChange={(event) => {setLoginPassword(event.target.value)}}
-                />
-                <i className="lock icon"></i>
-            </div>
-            </div>
-            <button 
-            className="ui blue submit button"
-            onClick={loginButton}
-            >Login</button>
-        </div>
-        </div>
-        <div className="middle aligned column">
-        <div className="ui form">
-            <div className="field">
-            <label>Email</label>
-            <div className="ui left icon input">
-                <input 
-                type="text" 
-                placeholder="Email" 
-                onChange={(event) => {setRegisterEmail(event.target.value)}}
-                />
-                <i className="user icon"></i>
-            </div>
-            </div>
-            <div className="field">
-            <label>Password</label>
-            <div className="ui left icon input">
-                <input 
-                type="password" 
-                onChange={(event) => {setRegisterPassword(event.target.value)}}
-                />
-                <i className="lock icon"></i>
-            </div>
-            </div>
-            <button 
-            className="ui blue submit button"
-            onClick={registerButton}
-            >Register</button>
-        </div>
-        </div>
-    </div>
-    <div className="ui vertical divider">
-        Or
-    </div>
-    </div>
+            <div className="signInBox">
 
-            <div className="getLoggedIn">
-                Please Login or Register to play..
+            <div className="loginBox">
+                <h3>Login here</h3>
+                <input 
+                type='text'
+                value={loginEmail}
+                placeholder='Email...' 
+                onChange={(event) => setLoginEmail(event.target.value)}
+                />
+                <br></br>
+                <br></br>
+                <input 
+                placeholder='Password...'
+                value='' 
+                onChange={(e) => setLoginPassword(e.target.value)}
+                />
+
+                <br></br>
+                <br></br>
+                <button 
+                id="loginButton"
+                onClick={loginButton}
+                >Login</button>
             </div>
 
+
+            <div className="registerBox">
+                <h3>Register here</h3>
+                <input 
+                value="text"
+                placeholder='Email...' 
+                onChange={(e) => setRegisterEmail(e.target.value)}
+                />
+                
+                <br></br>
+                <br></br>
+                <input 
+                value="text"
+                placeholder='Password...' 
+                onChange={(e) => setRegisterPassword(e.target.value)}
+                />
+
+                <br></br>
+                <br></br>
+                <button
+                onClick={registerButton}
+                >Create User</button>
+            </div>
+
+            </div>
+    
         </div>
     )
 }
